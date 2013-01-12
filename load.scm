cd796395f10ac8c79d6009d4fc1634bcd2880a54
Alexey Radul <axch@mit.edu>
2013-01-12 10:41:58 -0500

    And now there's a standard entry point, both to loading and to the test suite.

---
+++ load.scm
@@ -0,0 +1,30 @@
+;;; This file is part of Adaptive Plot, a library for plotting
+;;; functions from the MIT Scheme REPL
+;;; Copyright (C) 2010-2011 Alexey Radul
+;;;
+;;; Adaptive Plot is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU Affero General Public License
+;;; as published by the Free Software Foundation, either version 3 of
+;;; the License, or (at your option) any later version.
+;;;
+;;; Adaptive Plot is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Adaptive Plot.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+(define (self-relatively thunk)
+  (let ((place (ignore-errors current-load-pathname)))
+    (if (pathname? place)
+	(with-working-directory-pathname
+	 (directory-namestring place)
+	 thunk)
+	(thunk))))
+
+(define (load-relative filename)
+  (self-relatively (lambda () (load filename))))
+
+(load-relative "plotting")
