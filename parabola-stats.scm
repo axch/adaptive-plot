c3faa5d317e10424f7db3cb3f885965b043d5af3
Alexey Radul <axch@mit.edu>
2013-01-12 15:22:43 -0500

    Move the parabola geometry stuff to the file prepared for it.  Present in the opposite order, because I think that's clearer.

--- parabola-stats.scm
+++ parabola-stats.scm
@@ -35,4 +35,97 @@
 ;;;   coordinates of that point x on P that forms the triangle of
 ;;;   maximum area with the segment bounding that lobe?"
 ;;;
-;;; I solve this question by the strategy of iterative simplification.
+;;; Let us call the answer to this question the "stats" of that lobe
+;;; of that parabola.  I solve this question by the strategy of
+;;; iterative simplification: from the general case, move p2 to the
+;;; origin, then scale the vertical axis to make an obtuse parabola,
+;;; then rotate the desired lobe so that segment lies on the x axis,
+;;; and then the answer is obvious.
+
+;;; The stats of the given lobe of the parabola that goes through
+;;; (x1, y1), (x2, y2), and (x3, y3), under the condition that x1 < x2
+;;; < x3.  Legal values for LOBE are 'left for the p1,p2 lobe and
+;;; 'right for the p2,p3 lobe.
+(define (parabola-stats lobe x1 y1 x2 y2 x3 y3)
+  (receive
+   (area peak-x peak-y)
+   (origin-parabola-stats lobe
+    (- x1 x2) (- y1 y2) (- x3 x2) (- y3 y2))
+   (values area (+ peak-x x2) (+ peak-y y2))))
+
+;;; The stats of the given lobe of the parabola that goes through
+;;; (x1, y1), (0, 0), and (x3, y3), under the condition that x1 < 0 <
+;;; x3.  (The p1, 0, p3 angle may be acute.)
+(define (origin-parabola-stats lobe x1 y1 x3 y3)
+  (let* ((slope1 (/ y1 x1))
+	 (slope3 (/ y3 x3))
+	 (slope-max (max (abs slope1) (abs slope3))))
+    (if (< slope-max 2/3)
+	(obtuse-parabola-stats lobe x1 y1 x3 y3)
+	(receive
+	 (area peak-x peak-y)
+	 (obtuse-parabola-stats lobe
+	  x1 (/ y1 (* 2 slope-max)) x3 (/ y3 (* 2 slope-max)))
+	 (values (* area 2 slope-max) peak-x (* peak-y 2 slope-max))))))
+
+;;; The stats of the given lobe of the parabola that goes through
+;;; (x1, y1), (0, 0), and (x3, y3), under the condition that x1 < 0 <
+;;; x3 and the p1, 0, p3 angle is obtuse.
+(define (obtuse-parabola-stats lobe x1 y1 x3 y3)
+  (if (eq? lobe 'left)
+      (obtuse-parabola-left-stats x1 y1 x3 y3)
+      (obtuse-parabola-right-stats x1 y1 x3 y3)))
+
+
+;;; The stats of the p1,0 lobe of the parabola that goes through (x1,
+;;; y1), (0, 0), and (x3, y3), under the condition that x1 < 0 < x3
+;;; and the p1, 0, p3 angle is obtuse.
+(define (obtuse-parabola-left-stats x1 y1 x3 y3)
+  (let* ((dist-p1 (sqrt (+ (square x1) (square y1))))
+	 (cos-p1-angle (/ x1 dist-p1))
+	 (sin-p1-angle (/ y1 dist-p1))
+	 ;; rotation by -(angle - pi)
+	 (cos-rot-angle (- cos-p1-angle))
+	 (sin-rot-angle sin-p1-angle)
+	 (new-x3 (- (* cos-rot-angle x3) (* sin-rot-angle y3)))
+	 (new-y3 (+ (* sin-rot-angle x3) (* cos-rot-angle y3))))
+    (receive
+     (area peak-x peak-y)
+     (grounded-parabola-stats (- dist-p1) new-x3 new-y3)
+     ;; rotation back, by +(angle - pi)
+     (let ((old-peak-x (+ (* cos-rot-angle peak-x) (* sin-rot-angle peak-y)))
+	   (old-peak-y (+ (* -1 sin-rot-angle peak-x) (* cos-rot-angle peak-y))))
+       (values area old-peak-x old-peak-y)))))
+
+;;; The stats of the 0,p3 lobe of the parabola that goes through (x1,
+;;; y1), (0, 0), and (x3, y3), under the condition that x1 < 0 < x3
+;;; and the p1, 0, p3 angle is obtuse.
+(define (obtuse-parabola-right-stats x1 y1 x3 y3)
+  (let* ((dist-p3 (sqrt (+ (square x3) (square y3))))
+	 (cos-p3-angle (/ x3 dist-p3))
+	 (sin-p3-angle (/ y3 dist-p3))
+	 ;; rotation by -angle
+	 (new-x1 (+ (* cos-p3-angle x1) (* sin-p3-angle y1)))
+	 (new-y1 (+ (* -1 sin-p3-angle x1) (* cos-p3-angle y1))))
+    (receive
+     (area peak-x peak-y)
+     (grounded-parabola-stats dist-p3 new-x1 new-y1)
+     ;; rotation back, by +angle
+     (let ((old-peak-x (- (* cos-p3-angle peak-x) (* sin-p3-angle peak-y)))
+	   (old-peak-y (+ (* sin-p3-angle peak-x) (* cos-p3-angle peak-y))))
+       (values area old-peak-x old-peak-y)))))
+
+;;; The stats of the p1,0 lobe of the parabola that goes through
+;;; (x1, 0), (0, 0), and (x3, y), with the condition that x1 < 0 < x3
+;;; or x3 < 0 < x1.  The stats of such a parabola are the area between
+;;; the lobe and the axis, and the x and y corrdinates of the point
+;;; that takes the largest possible triangular bite out of it
+;;; (returned in that order).
+(define (grounded-parabola-stats x1 x3 y)
+  (let* ((base (abs x1))
+	 (peak-x (/ x1 2))
+	 (peak-y (/ (* -1 y peak-x peak-x)
+		    (* (- x3 x1) x3)))
+	 (area (abs (* 2/3 base peak-y))))
+    (values area peak-x peak-y)))
+
