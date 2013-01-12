2934d1ef79a48c4ac2d1037fd18a8b0d1d6f5fc3
Alexey Radul <axch@mit.edu>
2013-01-12 10:24:04 -0500

    Start a place for a unit test suite.

---
+++ test/load.scm
@@ -0,0 +1,19 @@
+;;; This file is part of Adaptive Plot, a library for plotting
+;;; functions from the MIT Scheme REPL
+;;; Copyright (C) 2010-2011 Alexey Radul
+;;;
+;;; Adaptive Plot is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU Affero General Public License
+;;; as published by the Free Software Foundation, either version 3 of
+;;; the License, or (at your option) any later version.
+;;;
+;;; Adaptive Plot is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Adaptive Plot.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+(load-relative "../testing/load")
