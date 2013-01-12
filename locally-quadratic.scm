9fd34f68688f6395985d21c77cef25d2398559a5
Alexey Radul <axch@mit.edu>
2013-01-12 16:15:38 -0500

    Arrangements and explanations.

--- locally-quadratic.scm
+++ locally-quadratic.scm
@@ -18,9 +18,71 @@
 
 (declare (usual-integrations))
 
+;;;; Evaluating piecewise linear approximations with locally quadratic
+;;;; approximations.
+
+;;; Any two consecutive segments of a piecewise linear curve can be
+;;; evaluated by comparing them to the parabola defined by those three
+;;; points.  The quality of a segment is given by how small a lobe the
+;;; parabola in question makes over it.  Any one segment can be
+;;; evaluated in one of two ways, using one of its two neighbors.  I
+;;; take the more critical of the two evaluations.
+
+;;; A `segment' record represents the segment between p1 and p2 of the
+;;; four points p0, p1, p2, and p3.  x0 < x1 < x2 < x3.  At most one
+;;; of p0 or p3 may be #f, indicating that the piecewise linear curve
+;;; ends with this segment.  There are one or two lobes over this
+;;; segment, one defined by p0 and one by p3 (if present).  The
+;;; candidate-area is the area of the larger, and the candidate-x is
+;;; the x-coordinate of the point that would take the biggest
+;;; triangular bite out of it.  An invariant of the geomtery is that
+;;; x1 < candidate-x < x2.  If candidate-area is 0, candidate-x may be
+;;; #f.
+(define-structure (segment safe-accessors (constructor %make-segment))
+  p0 p1 p2 p3 candidate-x candidate-area)
+
+(define (make-segment p0 p1 p2 p3)
+  (receive
+   (p0-area p0-x p0-y)
+   (if p0
+       (parabola-stats 'right
+        (car p0) (cdr p0) (car p1) (cdr p1) (car p2) (cdr p2))
+       (values 0 #f #f))
+   (receive
+    (p3-area p3-x p3-y)
+    (if p3
+	(parabola-stats 'left
+         (car p1) (cdr p1) (car p2) (cdr p2) (car p3) (cdr p3))
+	(values 0 #f #f))
+    (if (> p0-area p3-area)
+	(%make-segment p0 p1 p2 p3 p0-x p0-area)
+	(%make-segment p0 p1 p2 p3 p3-x p3-area)))))
+
+;;; Assuming x0 < x1 < new-x < x2 < x3, produces a list of two new
+;;; segments: x0 < x1 < new-x < x2 and x1 < new-x < x2 < x3.
+(define (split-segment segment new-p)
+  (let ((p0 (segment-p0 segment))
+	(p1 (segment-p1 segment))
+	(p2 (segment-p2 segment))
+	(p3 (segment-p3 segment)))
+    (list (make-segment p0 p1 new-p p2)
+	  (make-segment p1 new-p p2 p3))))
+
+;;; Segments are ordered by quality: good segments admit only small
+;;; candidate areas.
+(define (segment-quality-< seg1 seg2)
+  (cond ((< (segment-candidate-area seg1) (segment-candidate-area seg2))
+	 #f)
+	((> (segment-candidate-area seg1) (segment-candidate-area seg2))
+	 #t)
+	(else ; Arbitrary stable order
+	 (> (hash seg1) (hash seg2)))))
+
+(define segment-wt-tree-type (make-wt-tree-type segment-quality-<))
+
 (define (plot-line-interpolation-map relevant-points keep?)
   (let* ((relevant-segments
-	  (map make-segment (cons #f relevant-points) relevant-points 
+	  (map make-segment (cons #f relevant-points) relevant-points
 	       (cdr relevant-points) (append (cddr relevant-points) (list #f))))
 	 (meaningful-segments (filter keep? relevant-segments)))
     (alist->wt-tree segment-wt-tree-type
@@ -42,53 +104,3 @@
 	(if (null? insertees)
 	    tree
 	    (loop (wt-tree/add tree (car insertees) #t) (cdr insertees)))))))
-
-;;; This is the segment between p1 and p2 of the four points p0, p1,
-;;; p2, and p3.  x0 < x1 < x2 < x3.  At most one of p0 or p3 may be
-;;; #f.  There are one or two lobes over this segment, one defined by
-;;; p0 and one by p3 (each if present).  The candidate-area is the
-;;; area of the larger, and the candidate-x is the x-coordinate of the
-;;; point that would take the biggest triangular bite out of it.  An
-;;; invariant of the geomtery is that x1 < candidate-x < x2.  If
-;;; candidate-area is 0, candidate-x may be #f.
-(define-structure (segment safe-accessors (constructor %make-segment))
-  p0 p1 p2 p3 candidate-x candidate-area)
-
-(define (make-segment p0 p1 p2 p3)
-  (receive
-   (p0-area p0-x p0-y)
-   (if p0
-       (parabola-stats 'right
-        (car p0) (cdr p0) (car p1) (cdr p1) (car p2) (cdr p2))
-       (values 0 #f #f))
-   (receive
-    (p3-area p3-x p3-y)
-    (if p3
-	(parabola-stats 'left
-         (car p1) (cdr p1) (car p2) (cdr p2) (car p3) (cdr p3))
-	(values 0 #f #f))
-    (if (> p0-area p3-area)
-	(%make-segment p0 p1 p2 p3 p0-x p0-area)
-	(%make-segment p0 p1 p2 p3 p3-x p3-area)))))
-
-;;; 'less ignorable' is 'more obnoxious', but I need a < operation
-;;; because weight-balanced trees only do 'min'.
-(define (segment-ignorability-< seg1 seg2)
-  (cond ((< (segment-candidate-area seg1) (segment-candidate-area seg2))
-	 #f)
-	((> (segment-candidate-area seg1) (segment-candidate-area seg2))
-	 #t)
-	(else
-	 (> (hash seg1) (hash seg2)))))
-
-(define segment-wt-tree-type (make-wt-tree-type segment-ignorability-<))
-
-;;; Assuming x0 < x1 < new-x < x2 < x3, produces a list of two new
-;;; segments: x0 < x1 < new-x < x2 and x1 < new-x < x2 < x3.
-(define (split-segment segment new-p)
-  (let ((p0 (segment-p0 segment))
-	(p1 (segment-p1 segment))
-	(p2 (segment-p2 segment))
-	(p3 (segment-p3 segment)))
-    (list (make-segment p0 p1 new-p p2)
-	  (make-segment p1 new-p p2 p3))))
