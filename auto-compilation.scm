e38a4391cd7f8a9b0765ed1c75ffbd4d77cf0dcb
Alexey Radul <axch@mit.edu>
2013-01-13 18:01:32 -0500

    The program compiles reasonably (and cuts the running time of the test suite to 45 seconds; the test suite itself is still interpreted).

---
+++ auto-compilation.scm
@@ -0,0 +1,61 @@
+(declare (usual-integrations))
+
+(define (self-relatively thunk)
+  (if (current-eval-unit #f)
+      (with-working-directory-pathname
+       (directory-namestring (current-load-pathname))
+       thunk)
+      (thunk)))
+
+(define (load-relative filename #!optional environment)
+  (self-relatively (lambda () (load filename environment))))
+
+(define (compiled-code-type)
+  ;; Trying to support the C backend
+  (if (lexical-unbound?
+       (nearest-repl/environment)
+       'compiler:compiled-code-pathname-type)
+      "com"
+      (compiler:compiled-code-pathname-type)))
+
+;; The environment argument is the one to take macro definitions from
+;; for sf.
+(define (cf-conditionally filename #!optional environment)
+  (define (default-environment)
+    (if (current-eval-unit #f)
+        (current-load-environment)
+        (nearest-repl/environment)))
+  (if (default-object? environment)
+      (set! environment (default-environment)))
+  (fluid-let ((sf/default-syntax-table environment))
+    (sf-conditionally filename))
+  (if (cf-seems-necessary? filename)
+      (compile-bin-file filename)))
+
+(define (compiler-available?)
+  (not (lexical-unbound? (nearest-repl/environment) 'cf)))
+
+(define (compilation-seems-necessary? filename)
+  (or (sf-seems-necessary? filename)
+      (cf-seems-necessary? filename)))
+
+(define (sf-seems-necessary? filename)
+  (not (file-processed? filename "scm" "bin")))
+
+(define (cf-seems-necessary? filename)
+  (not (file-processed? filename "bin" (compiled-code-type))))
+
+(define (load-compiled filename #!optional environment)
+  (if (compiler-available?)
+      (begin (cf-conditionally filename environment)
+	     (load filename environment))
+      (if (compilation-seems-necessary? filename)
+	  (begin (warn "The compiler does not seem to be loaded")
+		 (warn "Are you running Scheme with --compiler?")
+		 (warn "Skipping compilation; loading source interpreted")
+		 (load (pathname-default-type filename "scm") environment))
+	  (load filename environment))))
+
+(define (load-relative-compiled filename #!optional environment)
+  (self-relatively (lambda () (load-compiled filename environment))))
+
